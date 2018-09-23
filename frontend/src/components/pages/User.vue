3515f4b97a1cb6555bc63d6f3cb01948d1920979
Michael74 <michael74.ru@mail.ru>
2018-09-23 21:06:38 +0500

    add Axio plugin

--- frontend/src/components/pages/User.vue
+++ frontend/src/components/pages/User.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
         <h1>User</h1>
+        <div v-for="user in users">
+            <ul>
+              <li>{{user.id}}</li>
+              <li>{{user.fio}}</li>
+              <li>{{user.email}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 
+import axios from "axios";
+
 export default {
     data () {
         return {
-            msg: 'Main'
+            users: null
         }
+    },
+    created() {
+        axios
+            .get('/admin/v1/user/')
+            .then(response => {
+                this.users = response.data;
+                console.log("response", this.users)
+              })
+            .catch(e => {
+                this.errors.push(e);
+                console.log("error", e)
+            });
     }
 }
 </script>
