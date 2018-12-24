79f721ab7d753054c963db0c61b2950c90e36a79
Stanislav <baryshnikovsv@gorky74.ru>
2018-12-24 15:44:49 +0500

    fix schedule

--- src/main/java/ru/soyuz_kom/config/Scheduling.java
+++ src/main/java/ru/soyuz_kom/config/Scheduling.java
@@ -22,25 +22,26 @@ public class Scheduling {
     @Scheduled(fixedRate = 1000)
     public void scheduler() throws InterruptedException {
         LocalDateTime now = LocalDateTime.now();
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formatDateTime = now.format(formatter);
 
-        System.out.println("Time : " + formatDateTime);
+        //System.out.println("Time : " + formatDateTime);
 
 
         List<Schedule> list= scheduleRepository.findAll();
 
         LocalDateTime dateTime = LocalDateTime.parse(list.get(0).getDateformat());
-        System.out.println("COUNT: " + list.size());
+        //System.out.println("COUNT: " + list.size());
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formatDateTime2 = dateTime.format(formatter2);
 
-
+        /**
+         * TODO:: проверка дат из задачи с текущем временем
+         */
         if(formatDateTime.equals(formatDateTime2)) {
-            System.out.println("WINNNNNNNNN");
+            //System.out.println("WINNNNNNNNN");
         }
 
-        System.out.println("Schedule: " + formatDateTime2);
+        //System.out.println("Schedule: " + formatDateTime2);
     }
 }
