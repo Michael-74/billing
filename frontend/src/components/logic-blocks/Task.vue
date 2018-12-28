8e7666077f45e2a37ebecf60b80d313261a37411
Stanislav <baryshnikovsv@gorky74.ru>
2018-12-28 12:48:36 +0500

    edit task component

--- frontend/src/components/logic-blocks/Task.vue
+++ frontend/src/components/logic-blocks/Task.vue
@@ -17,9 +17,29 @@
             </div>
             <div class="clear"></div>
             <br>
-            <h2 class="create__package-h2">Дополнительно</h2>
-            <div class="task__input">
-                <app-checkbox2 :data="task.rentWriteOff"></app-checkbox2>
+            <div class="task__extra">
+                <h2 class="create__package-h2 task__extra_left">Дополнительно</h2>
+                <div class="filters__header-right task__extra_right task__extra_margin-top">
+                    <button class="button button__less" @click="isExtraChange">
+                        {{ this.isExtra ? 'Свернуть' : 'Развернуть' }}
+                        <span class="filters__button-less-block">
+                                <font-awesome-icon class="filters__button-less" icon="signal"></font-awesome-icon>
+                            </span>
+                    </button>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="task__js-extra" v-show="isExtra">
+                <div class="task__input_block">
+                    <app-checkbox2 :data="task.rentWriteOff"></app-checkbox2>
+                </div>
+                <div class="task__input task__input_width">
+                    <app-checkbox2 :data="task.installments"></app-checkbox2>
+                </div>
+                <div class="task__input task__input_margin-top task__input_width">
+                    <app-input :data="task.priceInstallments"></app-input>
+                </div>
+                <div class="clear"></div>
             </div>
             <div class="clear"></div>
         </div>
@@ -49,11 +69,14 @@ export default {
     },
     data () {
         return {
+            isExtra: false,
             task: {
                 id: input('ID', 'ID', 'id', false, false, null, null),
                 name: input('Введите название', 'Введите название', 'name', true, false, null, null),
                 price: input('Введите сумму', 'Введите сумму', 'price', true, false, null, null),
-                rentWriteOff: checkbox2('Введите сумму', 'is_rent_write_off', true, false, null, true),
+                rentWriteOff: checkbox2('Списывать аренду при нулевом балансе', 'is_rent_write_off', true, false, null, false),
+                installments: checkbox2('Включить рассрочку', 'is_installments', true, false, null, false),
+                priceInstallments: input(null, 'Сумма в рассрочку', 'price_installments', true, false, null, null),
                 actionTime: actionTime('actionTime', false, null, {
                     day_start: null,
                     month_start: null,
@@ -64,7 +87,9 @@ export default {
         }
     },
     methods: {
-
+        isExtraChange: function () {
+            this.isExtra = !this.isExtra;
+        }
     },
     computed: {
 
@@ -91,7 +116,40 @@ export default {
         width: 230px;
         float: left;
     }
+    .task__input_width {
+        width: 180px;
+    }
+    .task__input_margin-top {
+        margin-top: -10px;
+    }
+    .task__input_block {
+        margin: 0 20px 30px 0;
+    }
     .task_margin-top {
         margin-top: -30px;
+    }
+
+    .filters__button-less-block {
+        position: relative;
+        display: inline-block;
+        width: 20px;
+        height: 12px;
+        overflow: hidden;
+    }
+    .filters__button-less {
+        position: absolute;
+        transform: rotate(-90deg);
+        top: 4px;
+        left: 0;
+        font-size: 14px;
+    }
+    .task__extra_right {
+        float: right;
+    }
+    .task__extra_left {
+        float: left;
+    }
+    .task__extra_margin-top {
+        margin-top: -12px;
     }
 </style>
